d15a62bb9bbb2e3f97d22294ac2bff96d0fa99d2
Anthon Castillo <anthoncastillo11@gmail.com>
2023-10-26 12:23:10 +0800

    updated Initial commit

--- script.js
+++ script.js
@@ -20,31 +20,46 @@ const answers = [
     "My sources say no.",
     "Outlook not so good.",
     "Very doubtful.",
-    "Anthon loves Gilay"
+  
 ];
+
 
 // Function to generate a random answer
 function generateAnswer() {
     const randomIndex = Math.floor(Math.random() * answers.length);
     return answers[randomIndex];
 }
+function apply3DEffect() {
+    const shakeButton = document.getElementById("shake-button");
+    shakeButton.style.transform = "perspective(100px) rotateX(20deg)";
+    setTimeout(() => {
+        shakeButton.style.transform = "perspective(100px) rotateX(10deg)";
+    }, 300);
+}
+
 function shakeMagicBall() {
     const magicBall = document.querySelector(".magic-ball");
+    const answers = document.querySelector(".answers");
     magicBall.classList.add("shake");
 
     setTimeout(() => {
         magicBall.classList.remove("shake");
-        magicBall.textContent = generateAnswer();
-    }, 500); // Adjust the duration (in milliseconds) to match your animation duration
+        answers.textContent = generateAnswer();
+
+    }, 1000); // Adjust the duration (in milliseconds) to match your animation duration
+
+        
+   
 }
+
 
 
 // Event listener for the "Shake" button
 document.getElementById("shake-button").addEventListener("click", () => {
     shakeMagicBall();
+    apply3DEffect();
 });
 
-
 // Initial answer display
-document.querySelector(".magic-ball").textContent = "Ask a question...";
+document.querySelector(".answers").textContent = "Ask a question...";
 
